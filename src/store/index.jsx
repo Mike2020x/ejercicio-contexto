71f191feec7b69abdde289507fed461e355b1114
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-06-23 11:11:17 -0500

    refactor:

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,40 +1,36 @@
 import { createContext, useReducer, useContext } from 'react';
-import reducer from './reducer';
-
 const VotacionContext = createContext();
 
+const initialState = {
+    totalVotos: [],
+    candidates: [
+        { id: 1, name: "sally", votes: 0 },
+        { id: 2, name: "molly", votes: 0 },
+        { id: 3, name: "lucy", votes: 0 },
+        { id: 4, name: "Lucas", votes: 0 }
+    ],
+    showResultType: "total", //--- percentaje
+}
+const reducer = (state, action) => {
+    switch (action.type) {
+
+    }
+    return state
+};
 // eslint-disable-next-line react/prop-types
 export const VotacionProvider = ({ children }) => {
-    const initialState = {
-        votos: [],
-        filtro: {
-            mostrarPorcentaje: false,
-            candidatosSeleccionados: []
-        }
-    };
-
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    const agregarVoto = (candidato) => {
-        const nuevoVoto = { candidato, id: state.votos.length + 1 };
-        dispatch({ type: 'AGREGAR_VOTO', payload: nuevoVoto });
-    };
-
-    const cambiarFiltro = (nuevoFiltro) => {
-        dispatch({ type: 'CAMBIAR_FILTRO', payload: nuevoFiltro });
-    };
-
-    const contextValue = {
-        state,
-        agregarVoto,
-        cambiarFiltro
-    };
-
     return (
-        <VotacionContext.Provider value={contextValue}>
+        <VotacionContext.Provider value={store}>
             {children}
         </VotacionContext.Provider>
     );
 };
 
-export const useVotacionContext = () => useContext(VotacionContext);
+export const useVoting = () => {
+    const store = useContext(VotacionContext)
+
+    if (!VotacionContext) {
+
+    }
+}
+
